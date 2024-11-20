92e0fa0f679c3a5498c41c24fe31438b1328e37f
Ensar5499 <ensarkulaksiz54@gmail.com>
2024-11-20 17:32:42 +0300

    Add files via upload
    kullanıcıdan alınan 2 deger arasındaki çift sayıları ve adedini bulur

---
+++ 2 deger arasındaki çift sayılar ve adedi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+          
+            {
+              
+                Console.WriteLine("İlk sayıyı giriniz:");
+                int sayi1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("İkinci sayıyı giriniz:");
+                int sayi2 = int.Parse(Console.ReadLine());
+
+                
+                int adet = 0;
+
+               
+                for (int i = sayi1; i <= sayi2; i++)
+                {
+                    if (i % 2 == 0) 
+                    {
+                        Console.WriteLine(i);  
+                        adet++;  
+                    }
+                }
+
+                
+                Console.WriteLine("Çift sayıların adedi: " + adet);
+
+                Console.ReadLine();  
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+    
